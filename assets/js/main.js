c70d68d7e51c55bd614a7915598d0b8fac58d657
Juliancio Carvalho <juliancio_alves@hotmail.com>
2022-07-22 10:58:00 -0300

    fix: scroll work

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,3 @@ sr.reveal('.skills__subtitle',{});
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
-
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
