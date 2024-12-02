7bf1d344d9791b5bdeda3efc66dcff10b6d5f165
Juliancio Carvalho <juliancio_alves@hotmail.com>
2024-12-02 18:40:04 -0300

    refact: projects

--- src/app/ui/projects/index.tsx
+++ src/app/ui/projects/index.tsx
@@ -2,148 +2,23 @@ import Image from "next/image";
 import styles from "./projects.module.css";
 import Link from "next/link";
 
+import projects from "@/mock/projects.json";
+
 export default function Projects() {
   return (
     <div className={styles.container} id="projects">
       <h1>Projetos</h1>
       <div className={styles.wrapper}>
-        <Link href="https://elegant-kepler-66d972.netlify.app/" target="_blank">
-          <Image
-            src="/projects/won-games.png"
-            alt="won games"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://keen-noether-ac5b35.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/dt-money.png"
-            alt="Finance app"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://charming-kataifi-101693.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/invoice-app.png"
-            alt="Invoice app"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://dazzling-borg-bbbc46.netlify.app/cards"
-          target="_blank"
-        >
-          <Image
-            src="/projects/pokemon.png"
-            alt="pokemon"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://incomparable-cat-034fc8.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/pomodoro.png"
-            alt="pomodoro app"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://cozy-concha-05b60e.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/lp-1.png"
-            alt="landing page"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://effervescent-pavlova-d1c649.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/todo-app.png"
-            alt="to do app"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://relaxed-youtiao-6fe424.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/lp-2.png"
-            alt="landing page"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://sprightly-cactus-ddee75.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/card.png"
-            alt="card"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://fluffy-genie-0919b5.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/review-page.png"
-            alt="review page"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://adorable-bunny-60f920.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/santa-fe-cell.png"
-            alt="Santa fe cell"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link
-          href="https://startling-dieffenbachia-52e184.netlify.app/"
-          target="_blank"
-        >
-          <Image
-            src="/projects/product-page.png"
-            alt="product page"
-            width={300}
-            height={200}
-          />
-        </Link>
-        <Link href="https://calcimport.netlify.app/" target="_blank">
-          <Image
-            src="/projects/calc-import.png"
-            alt="calc import"
-            width={300}
-            height={200}
-          />
-        </Link>
+        {projects.map((project, index) => (
+          <Link href={project.url} target="_blank" key={index}>
+            <Image
+              src={project.image}
+              alt={project.name}
+              width={300}
+              height={200}
+            />
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/app/ui/skills/index.tsx
+++ src/app/ui/skills/index.tsx
@@ -21,67 +21,67 @@ export default function Skills() {
       <h1>Skills</h1>
       <div className={styles.wrapper}>
         <div className={styles.skill}>
-          <FaHtml5 size={28} color="#4070f4" />
+          <FaHtml5 size={28} color="var(--secondary-color)" />
           <p>HTML5</p>
         </div>
         <div className={styles.skill}>
-          <FaCss3Alt size={28} color="#4070f4" />
+          <FaCss3Alt size={28} color="var(--secondary-color)" />
           <p>CSS3</p>
         </div>
         <div className={styles.skill}>
-          <IoLogoJavascript size={28} color="#4070f4" />
+          <IoLogoJavascript size={28} color="var(--secondary-color)" />
           <p>JavaScript</p>
         </div>
         <div className={styles.skill}>
-          <SiTypescript size={28} color="#4070f4" />
+          <SiTypescript size={28} color="var(--secondary-color)" />
           <p>TypeScript</p>
         </div>
         <div className={styles.skill}>
-          <SiWebpack size={28} color="#4070f4" />
+          <SiWebpack size={28} color="var(--secondary-color)" />
           <p>Webpack</p>
         </div>
         <div className={styles.skill}>
-          <FaReact size={28} color="#4070f4" />
+          <FaReact size={28} color="var(--secondary-color)" />
           <p>React.js</p>
         </div>
         <div className={styles.skill}>
-          <SiNextdotjs size={28} color="#4070f4" />
+          <SiNextdotjs size={28} color="var(--secondary-color)" />
           <p>Next.js</p>
         </div>
         <div className={styles.skill}>
-          <FaVuejs size={28} color="#4070f4" />
+          <FaVuejs size={28} color="var(--secondary-color)" />
           <p>Vue.js</p>
         </div>
         <div className={styles.skill}>
-          <SiRedux size={28} color="#4070f4" />
+          <SiRedux size={28} color="var(--secondary-color)" />
           <p>Redux</p>
         </div>
         <div className={styles.skill}>
-          <FaNodeJs size={28} color="#4070f4" />
+          <FaNodeJs size={28} color="var(--secondary-color)" />
           <p>Node.js</p>
         </div>
         <div className={styles.skill}>
-          <FaGitAlt size={28} color="#4070f4" />
+          <FaGitAlt size={28} color="var(--secondary-color)" />
           <p>Git</p>
         </div>
         <div className={styles.skill}>
-          <SiJest size={28} color="#4070f4" />
+          <SiJest size={28} color="var(--secondary-color)" />
           <p>Jest</p>
         </div>
         <div className={styles.skill}>
-          <GiOctopus size={28} color="#4070f4" />
+          <GiOctopus size={28} color="var(--secondary-color)" />
           <p>Testing Library</p>
         </div>
         <div className={styles.skill}>
-          <SiCypress size={28} color="#4070f4" />
+          <SiCypress size={28} color="var(--secondary-color)" />
           <p>Cypress</p>
         </div>
         <div className={styles.skill}>
-          <SiAwselasticloadbalancing size={28} color="#4070f4" />
+          <SiAwselasticloadbalancing size={28} color="var(--secondary-color)" />
           <p>Micro-frontend</p>
         </div>
         <div className={styles.skill}>
-          <SiTextpattern size={28} color="#4070f4" />
+          <SiTextpattern size={28} color="var(--secondary-color)" />
           <p>Test Driven Development (TDD)</p>
         </div>
       </div>
